0fc620bf6f55185f174093a00d40a2baa873b3df
MK-MK-MK-AGH <76408685+MK-MK-MK-AGH@users.noreply.github.com>
2021-01-11 11:59:53 +0100

    Add files via upload

--- Produkt.cpp
+++ Produkt.cpp
@@ -1,6 +1,64 @@
-#include "Produkt.h"
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <list>
+class Produkt 
+{
+	string nazwa;
+	double cena;
+	string opis;
+public:
+	Produkt() 
+	{
+		nazwa = "";
+		cena = 0;
+		opis = "";
+	}
+	Produkt(string nazwa, double cena, string opis)
+	{
+		this->nazwa = nazwa;
+		this->cena = cena;
+		this->opis = opis;
+	}
+	void zmiana_danych(string nazwa, double cena, string opis) 
+	{
+		this->nazwa = nazwa;
+		this->cena = cena;
+		this->opis = opis;
+	}
+};
 
-void Produkt::zmiana_danych() {
-	// TODO - implement Produkt::zmiana_danych
-	throw "Not yet implemented";
+class Magazyn 
+{
+	int pojemnosc;
+	string stan;
+	list<Produkt> spis;
+public:
+	Magazyn(int pojemnosc, string stan) 
+	{
+		this->pojemnosc = pojemnosc;
+		this->stan = stan;
+	}
+	void Zaktualizuj() 
+	{
+		ifstream f;
+		f.open("Magazyn.txt");
+		f.is_open();
+		string line;
+		int x = 0;
+		while (getline(f, line, ';'))
+		{
+			spis.
+		}
+	}
+	void Dodanie_Produktu(string nazwa, double cena, string opis)
+	{
+
+	}
+};
+void main() 
+{
+	new Magazyn(100, "cokolwiek");
 }
