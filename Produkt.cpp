48d383f8e8a87b1f7503298fa66e7fa37d9c0480
MK-MK-MK-AGH <76408685+MK-MK-MK-AGH@users.noreply.github.com>
2021-01-12 18:03:34 +0100

    Add files via upload

--- Produkt.cpp
+++ Produkt.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-using namespace std;
 #include <fstream>
 #include <string>
 #include <algorithm>
 #include <list>
+using namespace std;
 class Produkt 
 {
 	string nazwa;
 	double cena;
 	string opis;
 public:
+	friend bool operator==(const Produkt& T1, const Produkt& T2) 
+	{
+		return(T1.nazwa == T2.nazwa && T1.cena == T2.cena && T1.opis == T2.opis);
+	}
 	Produkt() 
 	{
 		nazwa = "";
@@ -28,6 +32,10 @@ public:
 		this->cena = cena;
 		this->opis = opis;
 	}
+	void Print() 
+	{
+		cout << "Nazwa " + nazwa + " Cena: " << cena << " Opis " + opis<<endl;
+	}
 };
 
 class Magazyn 
@@ -36,10 +44,15 @@ class Magazyn
 	string stan;
 	list<Produkt> spis;
 public:
+	list<Produkt> get_spis() 
+	{
+		return spis;
+	}
 	Magazyn(int pojemnosc, string stan) 
 	{
 		this->pojemnosc = pojemnosc;
 		this->stan = stan;
+		Zaktualizuj();
 	}
 	void Zaktualizuj() 
 	{
@@ -48,17 +61,74 @@ public:
 		f.is_open();
 		string line;
 		int x = 0;
-		while (getline(f, line, ';'))
+		while (getline(f, line))
 		{
-			spis.
+			int pos1 = line.find(';')+1;
+			int pos2 = line.substr(pos1).find(';')+pos1;
+			string a = line.substr(0, pos1-1);
+			string b = line.substr(pos1, pos2-pos1);
+			string c = line.substr(pos2+1);
+			spis.push_back(Produkt(a, stod(b), c));
 		}
+	}
+	bool Dostepnosc(Produkt przedmiot) 
+	{
+		list<Produkt>::iterator it;
+		for (it = spis.begin(); it != spis.end(); it++)
+		{
+			if((*it) == przedmiot)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	void Dodanie_Produktu(string nazwa, double cena, string opis)
 	{
-
+		spis.push_back(Produkt(nazwa,cena,opis));
+	}
+	void Dodanie_Produktu(Produkt przedmiot)
+	{
+		spis.push_back(przedmiot);
+	}
+	void Usuwanie_Produktu(string nazwa, double cena, string opis)
+	{
+		spis.remove(Produkt(nazwa, cena, opis));
+	}
+	void Print_list() 
+	{
+		list<Produkt>::iterator it;
+		for (it = spis.begin(); it != spis.end(); it++) 
+		{
+			(*it).Print();
+		}
 	}
 };
-void main() 
+class Domowienie
 {
-	new Magazyn(100, "cokolwiek");
-}
+	int ilosc;
+	Produkt item;
+public:
+	bool zloznie_zamowienia_na_towar() 
+	{
+		///?
+	}
+	void aktualizacja_stanu_magazynu(Magazyn Mag) 
+	{
+		for (int x = 0; x < ilosc; x++) 
+		{
+			Mag.Dodanie_Produktu(item);
+		}
+	}
+};
+//test
+//void main() 
+//{
+//	Magazyn m(100, "cokolwiek");
+//	m.Dodanie_Produktu("AAa", 21, "kkk");
+//	m.Print_list();
+//	list<Produkt> a = m.get_spis();
+//	m.Usuwanie_Produktu("AAa", 21, "kkk");
+//	m.Print_list();
+//
+//}
