efe6a1ede50ab21cb49e7a09d079fd4df4d52b1c
MK-MK-MK-AGH <76408685+MK-MK-MK-AGH@users.noreply.github.com>
2021-01-12 18:15:10 +0100

    Add files via upload

--- Produkt.h
+++ Produkt.h
@@ -1,15 +1,43 @@
-#ifndef PRODUKT_H
-#define PRODUKT_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <list>
+using namespace std;
 
-class Produkt {
-
-private:
+class Produkt
+{
 	string nazwa;
 	double cena;
 	string opis;
-
 public:
-	void zmiana_danych();
+	friend bool operator==(const Produkt& T1, const Produkt& T2);
+	Produkt();
+	Produkt(string nazwa, double cena, string opis);
+	void zmiana_danych(string nazwa, double cena, string opis);
+	void Print();
 };
 
-#endif
+class Magazyn
+{
+	int pojemnosc;
+	string stan;
+	list<Produkt> spis;
+public:
+	list<Produkt> get_spis();
+	Magazyn(int pojemnosc, string stan);
+	void Zaktualizuj();
+	bool Dostepnosc(Produkt przedmiot);
+	void Dodanie_Produktu(string nazwa, double cena, string opis);
+	void Dodanie_Produktu(Produkt przedmiot);
+	void Usuwanie_Produktu(string nazwa, double cena, string opis);
+	void Print_list();
+};
+class Domowienie
+{
+	int ilosc;
+	Produkt item;
+public:
+	bool zloznie_zamowienia_na_towar();
+	void aktualizacja_stanu_magazynu(Magazyn Mag);
+};
