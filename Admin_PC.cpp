0c515419d1febacecf4f6a9d21a82a61530c475e
Maczeks <76433203+Maczeks@users.noreply.github.com>
2021-01-12 21:01:30 +0100

    Add files via upload

---
+++ Admin_PC.cpp
@@ -0,0 +1,21 @@
+#include "Nag³ówek.hpp";
+using namespace std;
+
+int main() {
+	cout << "DOMOWIENIE TOWARU = 1" << endl;
+	int choice;
+	cin >> choice;
+	switch (choice) {
+	case 1:
+		Domowienie produkt;
+		string nazwa;
+		double cena;
+		string opis;
+		cout << "Podaj dane produktu: " << endl;
+		data_global.Zaktualizuj();
+		data_global;
+		break;
+	}
+	system("pause");
+	return 0;
+}
---
+++ Kasa.cpp
@@ -0,0 +1,9 @@
+#include "Nag³ówek.hpp";
+using namespace std;
+
+int main() {
+
+
+	system("pause");
+	return 0;
+}
