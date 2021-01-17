ed417858ee4ca1304ab9fff1cbcf457ad15dd589
MK-MK-MK-AGH <76408685+MK-MK-MK-AGH@users.noreply.github.com>
2021-01-17 22:06:20 +0100

    Add files via upload

---
+++ cos/Admin_PC.cpp
@@ -0,0 +1,238 @@
+#include "Nag³ówek.hpp";
+using namespace std; 
+
+
+int main() {
+
+	data_global.Zaktualizuj();
+	// 1
+	string nazwa;
+	double cena;
+	string opis;
+	Produkt* glob_prod;
+	Domowienie* glob_dom;
+	Decyzja zapis;
+	int liczba;
+	int ilosc;
+
+	// 2
+	string nazwa1;
+	double cena1;
+	string opis1;
+	Produkt* zwrot;
+	string imie;
+	string nazwisko;
+	string adres;
+	string dowod_zakupu;
+	Formularz formularz;
+	Decyzja decyzja;
+	string linia;
+	bool zgoda = false;
+	int licznik=0;
+	int choice1;
+	Dane_Klienta* klient;
+	string nazwa2;
+	double cena2;
+	string opis2;
+
+	int choice_pracownik;
+	int choice_kierownik;
+
+		string login_k = "admin";
+		string haslo_k = "12345";
+
+		string podane_l;
+		string podane_h;
+
+		cout << "Podaj login: ";
+		cin >> podane_l;
+		cout << "Podaj haslo: ";
+		cin >> podane_h;
+		cout << endl;
+
+		if (podane_l == login_k && podane_h == haslo_k) {
+			do {
+				cout << "===================================" << endl;
+				cout << "\t PANEL KIEROWNIKA" << endl;
+				cout << "===================================" << endl << endl;
+				cout << "DOMOWIENIE TOWARU = 1" << endl;
+				cout << "SPRAWDZENIE DOSTEPNOSCI TOWARU = 2 " << endl;
+				cout << "USUNIECIE TOWARU = 3 " << endl;
+				cout << "WYSWIETLENIE MAGAZYNU = 4 " << endl;
+				cout << "PRZEKAZANIE TOWARU NA MAGAZYN = 5 " << endl;
+				cout << "KONIEC PRACY = 0 " << endl;
+				cout << endl;
+
+				cin >> choice_kierownik;
+				system("cls");
+				switch (choice_kierownik) {
+					zgoda = false;
+				case 1:
+					cout << "Podaj ilosc towaru " << endl;
+					cin >> ilosc;
+					cout << "Podaj nazwe, cene oraz opis zamawianego produktu" << endl;
+					cin >> nazwa >> cena >> opis;
+					glob_prod = new Produkt(nazwa, cena, opis);
+					//zapis.zapis_do_pliku(*glob_prod);
+					glob_dom = new Domowienie(ilosc, *glob_prod);
+					glob_dom->zloznie_zamowienia_na_towar();
+					glob_dom->aktualizacja_stanu_magazynu();
+					//data_global.Zaktualizuj();
+					data_global.Print_list();
+					//data_global.Dodanie_Produktu(nazwa, cena, opis);
+					//data_global.Zaktualizuj();
+					break;
+				case 2:
+					cout << "Podaj nazwe, cene oraz opis produktu " << endl;
+					cin >> nazwa >> cena >> opis;
+					glob_prod = new Produkt(nazwa, cena, opis);
+
+					if (data_global.Dostepnosc(*glob_prod)) {
+						cout << "PRODUKT JEST DOSTEPNY" << endl;
+					}
+					else
+					{
+						cout << "PRODUKT NIEDOSTEPNY!!!!!" << endl;
+					}
+					break;
+				case 3:
+					cout << "Podaj nazwe produktu, opis oraz cene " << endl;
+					cin >> nazwa >> cena >> opis;
+					glob_prod = new Produkt(nazwa, cena, opis);
+					data_global.Usuwanie_Produktu(nazwa, cena, opis);
+					//albo
+					//data_global.Usuwanie_Produktu(*glob_prod);
+					//data_global.Zaktualizuj();
+					break;
+				case 4:
+					data_global.Print_list();
+					break;
+				case 5:
+					Pracownik pracownik;
+					cout << "Podaj nazwe, cene, opis produktu, ktory zostal zwrocony" << endl;
+					cin >> nazwa1 >> cena1 >> opis1;
+					zwrot = new Produkt(nazwa1, cena1, opis1);
+					pracownik.przekazanie_zwroconego_towaru_na_magazyn(*zwrot);
+					//data_global.Zaktualizuj();
+					break;
+				}
+				if ((choice_kierownik >= 1) && (choice_kierownik <= 6)) {
+					cout << endl;
+					system("pause");
+				}
+			} while (choice_kierownik);
+		}
+			///////////////////////////////////////////////////////////////////////////////////
+
+		else {
+			do {
+				cout << "==================================" << endl;
+				cout << "\t PANEL PRACOWNIKA" << endl;
+				cout << "==================================" << endl << endl;
+				cout << "PRZEKAZANIE TOWARU NA MAGAZYN = 1 " << endl;
+				cout << "FORMULARZ = 2 " << endl;
+				cout << "WYSWIETLENIE MAGAZYNU = 3 " << endl;
+				cout << "SPRAWDZENIE DOSTEPNOSCI TOWARU = 4 " << endl;
+				cout << "KONIEC PRACY = 0 " << endl;
+				cout << endl;
+
+				cin >> choice_pracownik;
+				switch (choice_pracownik) {
+
+				case 1:
+					Pracownik pracownik;
+					cout << "Podaj nazwe, cene, opis produktu, ktory zostal zwrocony" << endl;
+					cin >> nazwa1 >> cena1 >> opis1;
+					zwrot = new Produkt(nazwa1, cena1, opis1);
+					pracownik.przekazanie_zwroconego_towaru_na_magazyn(*zwrot);
+					break;
+
+				case 2:
+					cout << "Podaj nazwe, cene i opis produktu, ktorego dotyczy formularz: " << endl;
+					cin >> nazwa2;
+					cin >> cena2;
+					cin >> opis2;
+					glob_prod = new Produkt(nazwa2, cena2, opis2);
+
+					while (zgoda == false && licznik < 3) {
+						cout << "Podaj dane klienta: " << endl;
+						cin >> imie >> nazwisko >> adres;
+
+						klient = new Dane_Klienta(formularz.uzupelnienie_danych(imie, nazwisko, adres));
+
+						bool tmp;
+						tmp = decyzja.podjecie_decyzji(*klient);
+						decyzja.przekazanie_decyzji(tmp);
+
+						if (tmp == false) {
+							licznik += 1;
+							cout << "Czy chcesz odwolac sie od decyzji? Tak=1, Nie=2" << endl;
+							int wybor;
+							cin >> wybor;
+							switch (wybor) {
+							case 1:
+								decyzja.odwolanie_od_decyzji();
+								break;
+							case 2:
+								cout << "Dziekujemy za odpowiedz :)" << endl;
+								zgoda = true;
+								break;
+							}
+						}
+						else {
+							zgoda = true;
+						}
+					}
+					cout << "Podaj forme rekompensaty:  1=Zwrot pieniedzy  2=Nowy towar" << endl;
+					cin >> choice1;
+					switch (choice1) {
+					case 1:
+						cout << "Kwota zwrocona: ";
+						cout << glob_prod->cena << endl;
+						break;
+					case 2:
+						if (data_global.Dostepnosc(*glob_prod) == true) {
+							data_global.Usuwanie_Produktu(nazwa2, cena2, opis2);
+							cout << "Nowy towar wydany" << endl;
+						}
+						else {
+							cout << "Domowienie towaru" << endl;
+							glob_dom = new Domowienie(1, *glob_prod);
+							glob_dom->zloznie_zamowienia_na_towar();
+							glob_dom->aktualizacja_stanu_magazynu();
+							cout << "Nowy towar wydany" << endl;
+							data_global.Usuwanie_Produktu(nazwa2, cena2, opis2);
+						}
+						break;
+					}
+					break;
+				case 3:
+					data_global.Print_list();
+					break;
+				case 4:
+					cout << "Podaj nazwe, cene oraz opis produktu " << endl;
+					cin >> nazwa >> cena >> opis;
+					glob_prod = new Produkt(nazwa, cena, opis);
+
+					if (data_global.Dostepnosc(*glob_prod)) {
+						cout << "PRODUKT JEST DOSTEPNY" << endl;
+					}
+					else
+					{
+						cout << "PRODUKT NIEDOSTEPNY!!!!!" << endl;
+					}
+					break;
+				}
+				//system("cls");
+				if ((choice_pracownik >= 1) && (choice_pracownik <= 6)) {
+					cout << endl;
+					system("pause");
+				}
+
+			} while (choice_pracownik);
+		}
+
+	data_global.Zapisz();
+	return 0;
+}
+
---
+++ cos/Źródło.cpp
@@ -0,0 +1,347 @@
+#include "Nag³ówek.hpp"
+using namespace std;
+
+int Kasa::kod = 12345;
+int Karta::PIN = 1234;
+string Formularz::dowod = "dowod";
+
+Produkt::Produkt()
+{
+	nazwa = "";
+	cena = 0;
+	opis = "";
+}
+
+Produkt::Produkt(string nazwa, double cena, string opis)
+{
+	this->nazwa = nazwa;
+	this->cena = cena;
+	this->opis = opis;
+}
+
+void Produkt::zmiana_danych(string nazwa, double cena, string opis)
+{
+	this->nazwa = nazwa;
+	this->cena = cena;
+	this->opis = opis;
+}
+
+Magazyn::Magazyn(int pojemnosc, string stan)
+{
+	this->pojemnosc = pojemnosc;
+	this->stan = stan;
+}
+
+Magazyn data_global(1000, "");
+
+
+void Magazyn::Zaktualizuj()
+{
+	ifstream f;
+	f.open("Magazyn.txt");
+	f.is_open();
+	string line;
+	int x = 0;
+	while (getline(f, line))
+	{
+		int pos1 = line.find(';') + 1;
+		int pos2 = line.substr(pos1).find(';') + pos1;
+		string a = line.substr(0, pos1 - 1);
+		string b = line.substr(pos1, pos2 - pos1);
+		string c = line.substr(pos2 + 1);
+		spis.push_back(Produkt(a, stod(b), c));
+	}
+	f.close();
+}
+
+//
+//void Decyzja::zapis_do_pliku(Produkt p) {
+//	fstream plik;
+//	plik.open("Magazyn.txt", ios::out | ios::app);
+//	if (plik.good() == true)
+//	{
+//		plik << endl;
+//		plik << p.nazwa << ";";
+//		plik << p.cena << ";";
+//		plik << p.opis;
+//		plik.close();
+//	}
+//}
+
+bool Magazyn::Dostepnosc(Produkt przedmiot)
+{
+	list<Produkt>::iterator it;
+	for (it = spis.begin(); it != spis.end(); it++)
+	{
+		if ((*it) == przedmiot)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void Magazyn::Dodanie_Produktu(string nazwa, double cena, string opis)
+{
+	spis.push_back(Produkt(nazwa, cena, opis));
+}
+
+
+void Magazyn::Dodanie_Produktu(Produkt przedmiot)
+{
+	spis.push_back(przedmiot);
+}
+
+
+void Magazyn::Usuwanie_Produktu(string nazwa, double cena, string opis)
+{
+	int licznik=0;
+	list<Produkt>::iterator it;
+	for (it = spis.begin(); it != spis.end(); it++)
+	{
+		if ((*it).nazwa == nazwa) {
+			licznik++;
+		}
+	}
+	licznik--;
+	spis.remove(Produkt(nazwa, cena, opis));
+	for (int x = 0; x < licznik; x++)
+	{
+		spis.push_back(Produkt(nazwa, cena, opis));
+	}
+}
+
+void Magazyn::Usuwanie_Produktu(Produkt przedmiot)
+{
+	spis.remove(przedmiot);
+}
+
+
+void Magazyn::Print_list()
+{
+	list<Produkt>::iterator it;
+	for (it = spis.begin(); it != spis.end(); it++)
+	{
+		(*it).Print();
+	}
+}
+Domowienie::Domowienie(int ILOSC, Produkt p) : ilosc(ILOSC), item(p) {};
+
+void Domowienie::zloznie_zamowienia_na_towar()
+{
+	cout << "Zamowiono!" << endl;
+}
+
+void Domowienie::aktualizacja_stanu_magazynu()
+{
+	for (int x = 0; x < ilosc; x++)
+	{
+		data_global.Dodanie_Produktu(item);
+	}
+}
+
+void Produkt::Print()
+{
+	cout << "Nazwa: " << nazwa << " Cena: " << cena << " Opis: " << opis << endl;
+}
+
+Kasa::Kasa(Produkt p1) {
+	p1 = data;
+}
+
+void Karta::autoryzacja() {
+	cout << "Transakcja zaakceptowana!" << endl;
+}
+
+double Kasa::koszt_zamowienia(Produkt p1) {
+	cena = cena + p1.cena;
+	return cena;
+}
+
+void Kasa::skanowanie_produktu(Produkt p1) {
+	if (data_global.Dostepnosc(p1)) { // z klasy Magazyn
+		koszt_zamowienia(p1);
+		usuniecie_produktu(p1); // z klasy magazyn
+		lista_zakupow.push_back(p1);
+	}
+	else {
+		cout << "Brak produktu na magazynie !!" << endl;
+	}
+}
+
+void Kasa::wprowadzenie_kodu_rabatowego(int  code) {
+	if (code == kod) {
+		cena = cena * 0.9;
+	}
+	else {
+		cout << "Nieprawidlowy kod rabatowy !" << endl;
+	}
+}
+void Kasa::usuniecie_produktu(Produkt p1) {
+	koszt_zamowienia(p1); //nie wiem jak rozwiazac to zeby odjelo
+	data_global.Dodanie_Produktu(p1); //z klasy magazyn
+}
+
+void Kasa::anulowanie_zakupow(bool) {
+	list<Produkt>::iterator it;
+	for (it = data_global.spis.begin(); it != data_global.spis.end(); it++)
+	{
+		usuniecie_produktu(*it);
+	}
+}
+
+void Kasa::wydruk_paragonu() {
+	cout << cena;
+}
+
+Dane_Klienta::Dane_Klienta(string im = " ", string naz = " ", string ad = " ", string dow = " ") :Imie(im), Nazwisko(naz), Adres(ad), Dowod_zakupu(dow) {}
+
+bool Kasa::rozpoczecie(bool b) {
+	if (b) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+Platnosc::Platnosc(double Cena = 0.) : cena(Cena) {};
+Gotowka::Gotowka(double ile = 0) : ilosc(ile) {};
+Gotowka kwota_global(0);
+
+double Gotowka::zwroc_reszte(double koszt, double gotowka) {
+	double reszta;
+	reszta = gotowka - koszt;
+	return reszta;
+}
+
+bool Platnosc::zatwierdz() {
+	cout << "Wcisnij 1 jesli chcesz zaplacic gotowka , 2 jesli placisz karta" << endl;
+	int  wybor;
+	cin >> wybor;
+	double ilosc;
+	int pin;
+	switch (wybor)
+	{
+	case 1:
+		cout << "Podaj kwote jaka zamierzasz wplacic" << endl;
+		cin >> ilosc;
+		if (ilosc > cena) {
+			kwota_global.zwroc_reszte(cena, ilosc);
+		}
+		else if (ilosc == cena) {
+			return true;
+		}
+		else {
+			cout << "Brak wystarczajacej ilosci gotowki " << endl;
+			return false;
+		}
+	case 2:
+		cout << "WprowadŸ kod PIN" << endl;
+		cin >> pin;
+		for (int i = 0; i < 3; i++) {
+			if (pin == karta_global.PIN) {
+				cout << "Poprawna autoryzacja" << endl;
+				return true;
+			}
+			else {
+				cout << "Niepoprawny PIN " << endl;
+			}
+		}
+		return false;
+	}
+}
+
+bool Decyzja::podjecie_decyzji(Dane_Klienta klient) {
+
+	string prove = "dowod";
+
+	int choice;
+	if (klient.Dowod_zakupu == prove) {
+		cout << "Czy istnieja widoczne wady wynikajace ze zlego uzywania? 1=Nie 2=Tak" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			return true;
+			break;
+		case 2:
+			return false;
+			break;
+		}
+	}
+	else {
+		return false;
+	}
+}
+
+void Decyzja::przekazanie_decyzji(bool decy) {
+	if (decy == true) {
+		cout << "Twoja formularz zostal rozpatrzony pozytywnie!" << endl;
+		
+	}
+	else {
+		cout << "Twoj formularz zostal rozpatrzony negatywnie!" << endl;
+	}
+}
+
+void Decyzja::odwolanie_od_decyzji() {
+	string odwolanie;
+	cout << "Napisz odwolanie: ";
+	cin >> odwolanie;
+}
+
+Dane_Klienta Formularz::uzupelnienie_danych(string im, string naz, string adres) {
+	string dowod;
+	int choice;
+	cout << "Czy klient posiada dowod zakupu?   1=Tak   2=Nie" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "Dowod zakupu: ";
+		cin >> dowod;
+		cout << endl;
+		break;
+	case 2:
+		cout << "Brak mozliwosci zlozenia formularza" << endl;
+		break;
+	}
+	Dane_Klienta* dane;
+	dane = new Dane_Klienta(im, naz, adres, dowod);
+	return *dane;
+};
+
+void Pracownik::przekazanie_zwroconego_towaru_na_magazyn(Produkt data) {
+	data_global.Dodanie_Produktu(data);
+}
+
+double Pracownik::zwrot_pieniedzy(double koszt) {
+	return koszt;
+}
+
+void Pracownik::wydanie_nowego_towaru(Produkt data) {
+	data_global.Usuwanie_Produktu(data);
+}
+void Pracownik::przekazanie_formularza() {
+	cout << "Formularz zostal przekazany kierownikowi" << endl;
+}
+
+////nowe
+double Kasa::koszt_zamowienia(double p1) {
+	cena = cena + p1;
+	return cena;
+}
+
+void Magazyn::Zapisz()
+{
+	ofstream f;
+	f.open("magazyn.txt", ofstream::trunc);
+	if (f.good() == true)
+	{
+		list<Produkt>::iterator it;
+		for (it = spis.begin(); it != spis.end(); it++)
+		{
+			f << (*it).nazwa << ";" << (*it).cena << ";" << (*it).opis << endl;
+		}
+	}
+}
